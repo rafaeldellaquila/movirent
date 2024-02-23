1272239ffb4f8fe31443b7ed3dcd04a97373191f
rafaeldellaquila <rafael@dellaquila.dev>
2024-02-23 18:58:02 -0300

    :tada: start new vue project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1 className="text-3xl font-bold underline">Hello World</h1>
+  </main>
+</template>
