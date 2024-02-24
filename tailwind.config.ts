601505d02837341886a58ee2b5f2ddb3ed060bae
rafaeldellaquila <rafael@dellaquila.dev>
2024-02-24 05:07:54 -0300

    :lipstick: put new theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,10 @@ export default {
       colors: {
         primary: { main: '#2E1F7A', dark: '#1A162E' },
         secondary: { main: '#FEDE03', dark: '#FECC03' },
-        white: '#FFFEFA'
+        white: '#FFFEFA',
+        info: '#46C5EE',
+        error: '#EE4646',
+        success: '#9AEE46'
       },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans]
